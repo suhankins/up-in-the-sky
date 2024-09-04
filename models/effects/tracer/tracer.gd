628451f20550ffa2acd86b85ce02a3afb0b38d9e
suhankins <punishedbernadetta@niuitmo.ru>
2024-09-05 00:27:17 +0400

    updated tracers to be more simple

--- models/effects/tracer/tracer.gd
+++ models/effects/tracer/tracer.gd
@@ -1,15 +1,13 @@
-extends MeshInstance3D
+extends Node3D
 
 @onready var timer: Timer = $Timer
-@onready var material: ShaderMaterial = self.get_active_material(0)
-
-@export var time: float = 0.05
+@onready var material: ShaderMaterial = self.get_child(0).get_active_material(0)
 
 func _ready() -> void:
 	material.set_shader_parameter('progress', 0.0)
 
 func _process(_delta: float) -> void:
-	var progress = 1.0 - timer.time_left / time
+	var progress = 1.0 - timer.time_left / timer.wait_time
 	material.set_shader_parameter('progress', progress)
 	if timer.is_stopped():
-		self.get_parent().queue_free()
+		self.queue_free()
