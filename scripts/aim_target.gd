08eb84cf3be4319672fb90993b8970047c0abad4
suhankins <punishedbernadetta@niuitmo.ru>
2024-09-22 12:54:58 +0300

    aim target now accepts nodes

--- scripts/aim_target.gd
+++ scripts/aim_target.gd
@@ -2,26 +2,39 @@ class_name AimTarget extends Resource
 
 var target_position: Vector3
 var target_player: Player
+var target_node: Node3D
+
 
 static func aim_target_from_vector(vector: Vector3) -> AimTarget:
 	var new_aim_target = AimTarget.new()
 	new_aim_target.target_position = vector
 	return new_aim_target
 
+
 static func aim_target_from_player(player: Player) -> AimTarget:
 	var new_aim_target = AimTarget.new()
 	new_aim_target.target_player = player
 	return new_aim_target
+
+
+static func aim_target_from_node(node: Node3D) -> AimTarget:
+	var new_aim_target = AimTarget.new()
+	new_aim_target.target_node = node
+	return new_aim_target
+
 
 func get_position_to_shoot_at() -> Vector3:
 	if target_position:
 		return target_position
 	if target_player:
 		if target_player.is_crouching():
-			return target_player.crouching_raycast_origin.global_position 
+			return target_player.crouching_raycast_origin.global_position
 		else:
 			return target_player.standing_raycast_origin.global_position
+	if target_node:
+		return target_node.global_position
 	return Vector3.ZERO
+
 
 func get_spread_modifier() -> float:
 	var modifier = 1.0
