6e6520ef3d18a8c9f0a972728d163387addfe6b7
suhankins <punishedbernadetta@niuitmo.ru>
2024-09-23 10:12:38 +0300

    cart now rebakes navigation mesh after moving

--- models/cart.gd
+++ models/cart.gd
@@ -1,8 +1,26 @@
-class_name Cart extends Node3D
+class_name Cart extends RigidBody3D
 
 @export var covers: Array[Cover]
+@export var navigation_region: NavigationRegion3D
+var moved = false
+const STOPPED_THRESHOLD = 0.0002
 
 
 func enable_cover():
 	for cover in covers:
 		cover.enabled = true
+
+
+func _physics_process(_delta: float) -> void:
+	if (
+		not VectorHelper.every_component_abs_is_less_than(self.linear_velocity, STOPPED_THRESHOLD)
+		or not VectorHelper.every_component_abs_is_less_than(
+			self.angular_velocity, STOPPED_THRESHOLD
+		)
+	):
+		self.moved = true
+		return
+	if self.moved:
+		print_debug("region rebake called")
+		navigation_region.bake_navigation_mesh()
+		moved = false
--- scripts/vector_helper.gd
+++ scripts/vector_helper.gd
@@ -1,10 +1,17 @@
 class_name VectorHelper
 
+
 static func get_with_y(vector: Vector3, y: float = 0) -> Vector3:
-    return Vector3(vector.x, y, vector.z)
+	return Vector3(vector.x, y, vector.z)
+
 
 static func vector_angle(vector: Vector3) -> float:
-    return vector.signed_angle_to(Vector3(0, 0, -1), Vector3(0, -1, 0))
+	return vector.signed_angle_to(Vector3(0, 0, -1), Vector3(0, -1, 0))
+
 
 static func get_direction(from: Vector3, to: Vector3) -> Vector3:
-    return VectorHelper.get_with_y(to - from).normalized()
+	return VectorHelper.get_with_y(to - from).normalized()
+
+
+static func every_component_abs_is_less_than(vector: Vector3, number: float) -> bool:
+	return abs(vector.x) < number and abs(vector.y) < number and abs(vector.z) < number
