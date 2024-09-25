8e9fd174e5d033a206adf50ab81b28bda51a9cd2
suhankins <punishedbernadetta@niuitmo.ru>
2024-09-25 14:36:52 +0400

    fixed ui not connecting to player

--- ui/ui.gd
+++ ui/ui.gd
@@ -9,5 +9,9 @@ extends Control
 func _ready() -> void:
 	if not player:
 		push_error("No player assinged to UI!")
+	player.ready.connect(_on_player_ready)
+
+
+func _on_player_ready() -> void:
 	health_control.assign_player(player)
 	ammo_control.assign_player(player)
