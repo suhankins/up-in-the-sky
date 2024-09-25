59a9b7d5ae38ea7af7b471247105a2f7772dd0ee
suhankins <punishedbernadetta@niuitmo.ru>
2024-09-25 13:37:08 +0400

    refactoried NPCBlackboard with additional checks

--- btree/NPCBlackboard.gd
+++ btree/NPCBlackboard.gd
@@ -5,12 +5,30 @@ const LAST_PLAYER_POSITION = "last_player_position"
 const ALERTED = "alert"
 const LIST_OF_NPCS = "list_of_npcs"
 
+const TEAM_VALUES = [LAST_PLAYER_POSITION, ALERTED, LIST_OF_NPCS]
+
 
 func _ready() -> void:
 	set_value(DELTA, 1.0)
-	set_value(ALERTED, false)
-	set_value(LIST_OF_NPCS, [])
 
 
 func _physics_process(delta: float) -> void:
 	set_value(DELTA, delta)
+
+
+func set_value(key: Variant, value: Variant, blackboard_name: String = DEFAULT) -> void:
+	assert(
+		blackboard_name != DEFAULT or not TEAM_VALUES.has(key),
+		str("Key ", key, " can only be used with a non-default blackboard")
+	)
+	super.set_value(key, value, blackboard_name)
+
+
+func get_value(
+	key: Variant, default_value: Variant = null, blackboard_name: String = DEFAULT
+) -> Variant:
+	assert(
+		blackboard_name != DEFAULT or not TEAM_VALUES.has(key),
+		str("Key ", key, " can only be used with a non-default blackboard")
+	)
+	return super.get_value(key, default_value, blackboard_name)
